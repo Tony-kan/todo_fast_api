bab5d6bad77d80584ca22c483b870a2f9f637810
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-06-30 11:45:21 +0200

    created the update task functionality

--- config.py
+++ config.py
@@ -10,4 +10,5 @@ uri = os.getenv("MONGODB_URL")
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 db = client.todo_fast_api
-collection = db['todo_data']
+collection = db['todo_data']  # collection todo_table
+collection1 = db['waiting_table']  # collection1 = waiting table
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, APIRouter
 from config import collection
 from database.schemas import all_tasks, individual_task
 from database.models import Todo
+from bson.objectid import ObjectId
+from datetime import datetime
 
 app = FastAPI()
 todo_router = APIRouter()
@@ -18,6 +20,20 @@ async def create_task(new_task: Todo):
     try:
         res = collection.insert_one(dict(new_task))
         return {"status_code": 200, "id": str(res.inserted_id)}
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Error : {e}")
+
+
+@todo_router.put("/{task_id}")
+async def update_task(task_id: str, updated_task: Todo):
+    try:
+        id = ObjectId(task_id)
+        existing_task = collection.find_one({"id": id, "is_deleted": False})
+        if not existing_task:
+            return HTTPException(status_code=404, detail=f"Task does not exist")
+        updated_task.updated_at = datetime.timestamp(datetime.now())
+        res = collection.update_one({"id": id}, {"$set": dict(updated_task)})
+        return {"status_code": 200, "message": "Task updated successfully"}
     except Exception as e:
         return HTTPException(status_code=500, detail=f"Error : {e}")
 
